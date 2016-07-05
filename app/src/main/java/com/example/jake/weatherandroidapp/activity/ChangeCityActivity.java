1dd7fff368fb0e889275a308d48b4ef51c32cb0e
jake <jakewill@qq.com>
2016-07-05 17:12:25 +0800

    init
    init the project

---
+++ app/src/main/java/com/example/jake/weatherandroidapp/activity/ChangeCityActivity.java
@@ -0,0 +1,15 @@
+package com.example.jake.weatherandroidapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jake.weatherandroidapp.R;
+
+public class ChangeCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_city);
+    }
+}
